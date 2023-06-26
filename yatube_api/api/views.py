6304f3cd4ea6d3ba4dfa1a56303f477e2f781b01
Vladimir Tolstik <vltolstik@yandex.ru>
2023-06-26 20:01:11 +0300

    result

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, pagination, permissions, viewsets
 
 from posts.models import Group, Post
+
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (CommentSerializer, GroupSerializer,
                           PostSerializer, FollowSerializer)
